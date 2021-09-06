7ea70afa053a873ea253effe765e1fd98642c50e
Jsam88 <jordansam@sbcglobal.net>
2021-09-05 23:24:45 -0700

    Updates

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+
+});
+
+
+var delayInMilliseconds = 2000;
+
+document.addEventListener("keyup", function(event) {
+    if (event.code === 'Enter') {
+        var element = document.getElementById("WaitingGif");
+        element.classList.add("fade-out");
+        setTimeout(function() {
+            element.classList.add("hidden");
+        }, delayInMilliseconds);
+        // element.style.display = "none";
+}
+    });
